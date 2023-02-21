c0454d3f3db182d0dc825da28603161249ebce32
V D Panduranga Sai Guptha <96918090+saiguptha2003@users.noreply.github.com>
2023-02-21 07:01:19 +0530

    Add files via upload

---
+++ averageNumber.sh
@@ -0,0 +1,11 @@
+number=$1
+#echo -n "enter number of elements :"
+#read number
+sum=0
+for (( i=0 ; i<$number ; i++ ));
+do 
+read elements
+sum=`expr $sum+$elements`
+done
+average=$(( sum / number ))
+echo "averge is n numbers :$average "
